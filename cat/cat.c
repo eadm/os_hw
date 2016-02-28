0beebe5950d9faca3d3ac50aab481c4f03c97d6c
Ruslan Davletshin <smartmorefeed@gmail.com>
2016-02-28 20:00:18 +0300

    2 HW

---
+++ cat/cat.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int main() {
+    const int n = 1024;
+
+    char buf[n];
+
+    int i = 0;
+    while ((i = read(STDIN_FILENO, buf, n)) > 0) {
+        write(STDOUT_FILENO, buf, i);
+    }
+    return 0;
+}
