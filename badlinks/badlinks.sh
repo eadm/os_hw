9e2946c60b13b21321d059e03bbec6e4e6ee0e88
Ruslan Davletshin <smartmorefeed@gmail.com>
2016-03-21 14:25:05 +0300

    hw 2

---
+++ badlinks/badlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR="$1/*"
+TIME="+7"
+
+FILES=`find $DIR -ctime $TIME`
+
+for f in $FILES
+do
+	if test -L $f; then
+		#echo "$f is symlink"
+		if [ ! -e $f ]; then
+			echo "$f"
+		#else
+		#	echo "Bad"
+		fi
+	#else 
+	#	echo "$f isn't symlink"
+	fi
+done
+
