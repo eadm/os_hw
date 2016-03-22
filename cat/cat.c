b49a3f99c20db254f18b8e19a2e19faa0f319c3c
Ruslan Davletshin <smartmorefeed@gmail.com>
2016-03-22 11:23:42 +0300

    def 2

--- cat/cat.c
+++ cat/cat.c
@@ -8,10 +8,15 @@ int main(int argc, char* argv[]) {
 	const int n = 1024;
 	
 
-	int fd = (argc > 1) ? open(argv[1], O_RDONLY) : STDIN_FILENO;
+	int fd = STDIN_FILENO; //(argc > 1) ? open(argv[1], O_RDONLY) : STDIN_FILENO;
     	char buf[n];
-
-
+	
+	int wd = STDOUT_FILENO;
+	if (argc > 1) {
+		wd = open(argv[1], O_CREAT | O_WRONLY, 0777);
+		//close(cd);
+		//wd = open(argv[1], O_WRONLY);  
+	}
 
 	int i = 0;
     	while ((i = read(fd, buf, n)) != 0) {
@@ -27,7 +32,7 @@ int main(int argc, char* argv[]) {
 
 		int offset = 0;
 		while (offset < i) {
-			int w = write(STDOUT_FILENO, buf + offset, i - offset);
+			int w = write(wd, buf + offset, i - offset);
 			
 			if (w == -1) {		
 				if (errno == EINTR) {
@@ -42,6 +47,6 @@ int main(int argc, char* argv[]) {
 		}	
         }
 	
-	if(argc > 1) close(fd);
+	if(argc > 1) close(wd);
     	return 0;
 }
