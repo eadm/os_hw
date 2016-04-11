b6af3f12eda8dd485b1d6aef9b16bd2491f17632
Ruslan Davletshin <smartmorefeed@gmail.com>
2016-04-11 18:05:47 +0300

    finally working pipes

--- midtermsh/midtermsh.cpp
+++ midtermsh/midtermsh.cpp
@@ -1,190 +1,150 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <vector>
 
-char ** split(char * buff, int n, int *& offsets, char d, int& length, int ending) {
-    int size = 10;
-    char ** tmp = (char **) malloc (sizeof(char*) * size);
-    
-    int * offs = (int *) malloc (sizeof(int) * size);
-    
-    length = 0;
-    int i = 0;
-    int offset = 0;
-    while (i + offset < n) {
-        if (buff[i + offset] == d) {
-            if (offset == 0) {
-                i++;
-                continue;
-            }
-            
-            if (length + 1 == size) {
-                char** ttt = (char **) malloc (sizeof(char*) * size * 2);
-                memcpy(ttt, tmp, size);
-                free(tmp);
-                tmp = ttt;
-                
-                int* t_offs = (int *) malloc (sizeof(int) * size * 2);
-                memcpy(t_offs, offs, size);
-                free(offs);
-                offs = t_offs;
-                
-                size *= 2;
-            }
-            
-            tmp[length] = (char*) malloc (sizeof(char) * offset);
-            offs[length] = offset;
-            memcpy(tmp[length++], buff + i, offset);
-            
-            i += offset ;
-            offset = 0;
-        } else {
-            offset++;
-        }
-    }
-    
-    if (offset - 1 >= 0) {
-        if (length + 1 == size) {
-            char** ttt = (char **) malloc (sizeof(char*) * size * 2);
-            memcpy(ttt, tmp, size);
-            free(tmp);
-            tmp = ttt;
-            
-            int* t_offs = (int *) malloc (sizeof(int) * size * 2);
-            memcpy(t_offs, offs, size);
-            free(offs);
-            offs = t_offs;
-        }
-        
-        tmp[length] = (char*) malloc (sizeof(char) * offset);
-        offs[length] = offset;
-        memcpy(tmp[length++], buff + i, offset - 1 + ending);
-    }
-    
-    if (ending == 1) {
-        tmp[length++] = (char*)0;
-    }
-    
-    offsets = offs;
-    return tmp;
-}
+using namespace std;
 
 int pid = 0;
-int running = 0;
-
-//int pipes[2];
-
-int execute(char ** commands, int length, int pos) {
-    pid = fork();
-    if(pid == 0) {
-//        if (pos == length - 1) {
-//            dup2(pipes[0], STDOUT_FILENO);
-//        }
-        execvp(commands[0], commands);
-        return 0;
-    } else {
-        running = 1;
-        wait(&pid);
-        running = 0;
-    }
-    for (int i = 0; i < length; i++) {
-        free(commands[i]);
-    }
-    
-    free(commands);
-    return 1;
-}
+bool running = false;
 
 void handler(int s) {
     if (s != SIGINT) return;
-    
-    if (running == 1) {
+    if (running) {
         kill(pid, SIGINT);
     } else {
-//        exit(0);
+        exit(0);
     }
+}
+
+char* append(char * next, char * buff, ssize_t r) {
+    size_t next_len = (next == NULL) ? 1 : strlen(next) + 1;
+    size_t tmp_len = next_len + r;
+    char* tmp = (char*) malloc (sizeof(char) * tmp_len);
+    tmp[0] = '\0'; // говорим C, что начало строки в начале строки
+
+
+    if (next != NULL && next_len > 1) { // тк если next_len == 1, то там только \n
+        strcat(tmp, next);
+    }
+    if (r > 0) strcat(tmp, buff);
+    return tmp;
+}
+
+char* get_current_line(char*& next, char* buff, ssize_t r) {
+    char* tmp = append(next, buff, r);
+    next = strchr(tmp, '\n');
+
+    if (next != NULL) { // помечаем конец строки
+        tmp[next - tmp] = '\0';
+        next++;
+    }
+
+    return tmp;
+}
+
+vector<char*> split(char* line, char d) {
+    vector<char*> result;
+    char* next;
+
+    while ((next = strchr(line, d)) != NULL) {
+        line[next - line] = '\0';
+        if (next - line > 0) result.push_back(line);
+        line = next + 1;
+    }
+    if (strlen(line) > 0) result.push_back(line);
+
+    return result;
+}
+
+int pipes1[2];
+int pipes2[2];
+
+int execute(char ** commands, bool isFirst, bool isLast) {
+    pipe(pipes2);
+    pid = fork();
+    if(pid == 0) {
+        if (!isFirst) {
+            dup2(pipes1[0], 0);
+            close(pipes1[0]);
+            close(pipes1[1]);
+        }
+        if (!isLast) {
+            dup2(pipes2[1], 1);
+        }
+        close(pipes2[0]);
+        close(pipes2[1]);
+        execvp(commands[0], commands);
+        return 0;
+    } else {
+        if (!isFirst) {
+            close(pipes1[0]);
+            close(pipes1[1]);
+        }
+        if (isLast) {
+            close(pipes2[0]);
+            close(pipes2[1]);
+        }
+    }
+    pipes1[0] = pipes2[0];
+    pipes1[1] = pipes2[1];
+    return 1;
+}
+
+void execute_line(char * line) {
+    vector<char *> pipers = split(line, '|');
+    size_t count = 0;
+    for (auto c : pipers) {
+        vector<char*> commands = split(c, ' ');
+        commands.push_back(NULL); // покажем C, что массив закончился
+        execute(commands.data(), count == 0, count == pipers.size() - 1);
+        count++;
+    }
+    running = true;
+    int stat;
+    while ((pid = wait(&stat)) > 0); // ждем пока все закончится
+    running = false;
 }
 
 int main() {
     const int buff_size = 1024;
-    
+//    const char* welcome = "\x1B[35m$\x1B[0m ";
+    const char* welcome = "$ ";
     signal(SIGINT, &handler);
-    
-//    struct sigaction sa;
-//    sa.sa_handler = &handler;
-//    sigaction(SIGINT, &sa, NULL);
-    
+
     char* buff = (char*)malloc(sizeof(char) * buff_size);
+    write(STDOUT_FILENO, welcome, strlen(welcome));
     
-    int r;
-    
-    write(STDOUT_FILENO, "$ ", 2);
-    
-    int line_length = 0;
-    char* line;
+    ssize_t r;
+    char* next = NULL;
     
     while ((r = read(STDIN_FILENO, buff, buff_size)) != 0) {
-        
         if (r == -1) {
             if (errno == EINTR) {
+                perror("EINTR caught\n");
                 continue;
             } else {
-                break;
+                return 1;
             }
         }
-        
-        
-        // reading long commmands
-        char* line_t = (char*) malloc (sizeof(char) * (line_length + r));
-        memcpy(line_t, line, line_length);
-        memcpy(line_t + line_length, buff, r);
-
-        line = line_t;
-        line_length += r;
-        
         if (buff[r - 1] != '\n') {
+            next = append(next, buff, r);
             continue;
         }
-        
-        int pipers_len = 0;
-        int *lens;
-        
-        char ** pipers = split(line, line_length, lens, '|', pipers_len, 0);
 
-        
-//        pipe(pipes);
-        for (int i = 0; i < pipers_len; i++) {
-            int *__ss;
-            int length = 0;
-            
-//            if (i != pipers_len - 1) {
-//                dup2(pipes[0], STDOUT_FILENO);
-//            } else {
-//                dup2(1, STDOUT_FILENO);
-//            }
-            
-            char ** command = split(pipers[i], lens[i], __ss, ' ', length, 1);
-            execute(command, length, i);
-            free(__ss);
-            free(pipers[i]);
-            
-//            if (i != 0) {
-//                dup2(pipes[1], STDIN_FILENO);
-//            }
-//            dup2(pipes[0], STDOUT_FILENO);
-        }
-        free(pipers);
-        
-        
-        free(line);
-        line_length = 0;
-        
-        write(STDOUT_FILENO, "$ ", 2);
+        buff[r] = '\0';
+        execute_line(get_current_line(next, buff, r));
+        write(STDOUT_FILENO, welcome, strlen(welcome));
     }
+
+    while (next != NULL && strlen(next) > 1) {
+        execute_line(get_current_line(next, buff, 0));
+        write(STDOUT_FILENO, welcome, strlen(welcome));
+    }
+
     free(buff);
-    
     return 0;
 }
