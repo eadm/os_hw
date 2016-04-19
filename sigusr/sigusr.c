91014470f159e1015b5fdaef314f3387883f232e
Ruslan Davletshin <smartmorefeed@gmail.com>
2016-04-19 11:58:48 +0300

    hw3

--- sigusr/sigusr.c
+++ sigusr/sigusr.c
@@ -2,25 +2,46 @@
 #include <unistd.h>
 #include <signal.h>
 
-void handler(int s, siginfo_t * sinfo, void * v) {
-	switch(s) {
-		case SIGUSR1:
-			printf("SIGUSR1");
-		break;
-		case SIGUSR2:
-			printf("SIGUSR2");
-		break;
-	}
-	printf(" from %d\n", sinfo->si_pid);
+pid_t pid = 0;
+int s = 0;
+
+void handler(int sig, siginfo_t * sinfo, void * v) {
+//    printf("CCC");
+//    flag = 1;
+//	switch(s) {
+//		case SIGUSR1:
+//			printf("SIGUSR1");
+//		break;
+//		case SIGUSR2:
+//			printf("SIGUSR2");
+//		break;
+//	}
+//    sleep(10);
+    s = sig;
+    pid = sinfo->si_pid;
 }
 
 int main() {
 	struct sigaction sa;
 		
 	sa.sa_sigaction = &handler;
-	sigaction(SIGUSR1, &sa, NULL);	
-	sigaction(SIGUSR2, &sa, NULL);	
+    
+    sigset_t block;
+    sigemptyset (&block);
+    for (int i = 1; i < 32; i++) {
+        sigaddset (&block, i);
+    }
+    
+    sa.sa_mask = block;
+    sa.sa_flags = 0;
+    for (int i = 1; i < 32; i++) {
+        sigaction(i, &sa, NULL);
+    }
 
-	if (sleep(10) == 0) printf("No signals were caught\n");
+    if (sleep(10) == 0) {
+        printf("No signals were caught\n");
+    } else {
+        printf("%d from %d\n", s, pid);
+    }
 	return 0;
 }	
